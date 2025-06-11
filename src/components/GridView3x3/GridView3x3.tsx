7d4c5ed361473d499e2ab831addca8518a397d76
TomHung <tomworkpurpose@gmail.com>
2025-06-11 11:50:29 +0800

    Refactor GridView3x3 to manage card state with localStorage; implement loading and saving of cards

--- src/components/GridView3x3/GridView3x3.tsx
+++ src/components/GridView3x3/GridView3x3.tsx
@@ -12,30 +12,56 @@ const CARD_COUNTS = 9;
 // Cards default colors
 const CARD_COLORS = {
   main: "#f04902",
-  secondary: "#ffa15c",
-  tertiary: "#ffcaa4",
+  secondary: "#fecd5e",
+  tertiary: "#fed879",
 };
 
+// LocalStorage key
+const STORAGE_KEY = "mandala-chart-3x3-cards";
+
 // Default cards
-const initialCards: CardData[] = Array(CARD_COUNTS)
-  .fill(null)
-  .map((_, index) => ({
-    id: `card-${index}`,
-    title: index === CENTER_CARD_INDEX ? "核心目標" : `關聯目標`,
-    content: `說明...`,
-    bgColor:
-      index === CENTER_CARD_INDEX
-        ? CARD_COLORS.main
-        : [1, 3, 5, 7].includes(index)
-        ? CARD_COLORS.secondary
-        : CARD_COLORS.tertiary,
-  }));
+const getInitialCards = (): CardData[] =>
+  Array(CARD_COUNTS)
+    .fill(null)
+    .map((_, index) => ({
+      id: `card-${index}`,
+      title: index === CENTER_CARD_INDEX ? "核心目標" : `關聯目標`,
+      content: `說明...`,
+      bgColor:
+        index === CENTER_CARD_INDEX
+          ? CARD_COLORS.main
+          : [1, 3, 5, 7].includes(index)
+          ? CARD_COLORS.secondary
+          : CARD_COLORS.tertiary,
+    }));
+
+// Load cards from localStorage
+const loadCardsFromStorage = (): CardData[] => {
+  const savedCards = localStorage.getItem(STORAGE_KEY);
+  if (savedCards) {
+    const parsedCards = JSON.parse(savedCards);
+    if (parsedCards) {
+      return parsedCards;
+    }
+  }
+
+  return getInitialCards();
+};
+
+// Save cards to localStorage
+const saveCardsToStorage = (cards: CardData[]) => {
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(cards));
+};
 
 export const GridView3x3 = () => {
-  const [cards, setCards] = useState<CardData[]>(initialCards);
+  const [cards, setCards] = useState<CardData[]>(loadCardsFromStorage);
   const [editIndex, setEditIndex] = useState<number | null>(null);
   const [dragIndex, setDragIndex] = useState<number | null>(null);
   const [modalPosition, setModalPosition] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    saveCardsToStorage(cards);
+  }, [cards]);
 
   const handleDragStart = (index: number) => {
     setDragIndex(index);
@@ -90,6 +116,7 @@ export const GridView3x3 = () => {
 
   useEffect(() => {
     const handleReset = () => {
+      const initialCards = getInitialCards();
       setCards(initialCards);
       setEditIndex(null);
     };
